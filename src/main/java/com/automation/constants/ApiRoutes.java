a781f8bdd874af20d7cfbb6e4f9572e2a793663e
Jorge Fereira Castellano <134786134+jorgefere@users.noreply.github.com>
2026-09-18 17:52:38 +0200

    Build Rest Assured API automation framework

---
+++ src/main/java/com/automation/constants/ApiRoutes.java
@@ -0,0 +1,2 @@
+package com.automation.constants;
+public final class ApiRoutes { public static final String USERS="/users"; public static final String USER_BY_ID="/users/{userId}"; private ApiRoutes(){} }
